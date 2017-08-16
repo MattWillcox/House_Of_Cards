6b2791372280195ba2af7ab2924b477fa451e5ff
Matt Willcox <mattjwillcox@gmail.com>
2017-08-16 02:21:04 +0000

    Fixes seed file, adds post route to users.js and adds .DS_Store to gitignore

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const router  = express.Router();
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({extended: true}));
 
 module.exports = (knex) => {
 
@@ -12,6 +15,13 @@ module.exports = (knex) => {
       .then((results) => {
         res.json(results);
     });
+  });
+
+  router.post("/", (req, res) => {
+    knex('users')
+      .insert({name: `"${req.body.name}"`})
+      .then(() => res.status(200).send(req.body.name))
+      .catch(function(error) { console.error(error); });
   });
 
   return router;
