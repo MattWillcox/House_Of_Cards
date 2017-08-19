12b6b2b4865fe1e2501f115f70f4fe001267bcd7
Ricky <Na_ricky@hotmail.com>
2017-08-19 17:31:07 +0000

    refractor stage 1

--- public/scripts/goofspiel.js
+++ public/scripts/goofspiel.js
@@ -1,6 +1,116 @@
-// $(() => {
+ $(() => {
 
-// $(".clickable").click(function() {
-//     $(this).hide();
-//   });
-// });
+ var socket = io.connect('http://localhost:8080/');
+    var playerNum = Number(/userId=(\d+)/.exec(document.cookie)[1]);
+
+ renderCardFaces([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+
+
+    function cardPlay(index) {
+      console.log("cardplay.  emitting to server.");
+      socket.emit('cardplay', JSON.stringify({
+        index: index,
+        cookies: document.cookie
+      }));
+    }
+    socket.on('load', function(state) {
+          var frontClasses = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
+      var state = JSON.parse(state);
+      $('<div>').addClass('card spades clickable ' + frontClasses[state.prizeCard - 1])
+      .appendTo('.card.stack');
+    });
+
+    socket.on('update state', function(data) {
+      var state = JSON.parse(data);
+      console.log("server says new state is", state);
+
+    function countNulls(p1, p2) {
+      let p1cards = 0;
+      let p2cards = 0;
+
+      for(card in p1){
+        if(p1[card] !== null){
+          p1cards++;
+        }
+
+      }
+      for(card in p2){
+        if(p2[card] !== null){
+          p2cards++;
+        }
+      }
+      return p1cards - p2cards;
+    }
+      console.log('outside', playerNum);
+      console.log("difference", countNulls(state.p1Hand, state.p2Hand));
+      if (playerNum === 1) {
+        if(countNulls(state.p1Hand, state.p2Hand) >= -1){
+          console.log("p1 cards");
+          renderCardFaces(state.p1Hand);
+        }
+      } else {
+        console.log('inside', playerNum);
+        if(countNulls(state.p1Hand, state.p2Hand) <= 1){
+          console.log("p2 cards");
+        renderCardFaces(state.p2Hand);
+      }
+    }
+
+    });
+
+    function shuffleCards(state) {
+      deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+      var m = deck.length, t, i;
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = deck[m];
+        deck[m] = deck[i];
+        deck[i] = t;
+      }
+
+      let prizeCard = deck.pop();
+      const nextState = {
+        gameState: 'SHUFFLED',
+        p1Hand: state.p1Hand,
+        p2Hand: state.p2Hand,
+        prizeDeck: deck,
+        prizeCard: prizeCard,
+        p1Score: state.p1Score,
+        p2Score: state.p2Score
+      };
+
+      return nextState;
+    }
+
+    function updateState(data) {
+      // update ui based on data object
+      //$().appendChild();
+      console.log(data);
+    }
+
+    function renderCardFaces(myHand){
+      var frontClasses = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
+      for (let i = 0; i < 13; i++) {
+        var cardId = '#mine' + i;
+        var card = $(cardId);
+        function sendThisCard() {
+          cardPlay(i);
+        }
+        card.off('click');    // NOTE: if we ever need a second click handler on this, this line might be bad news
+        if (myHand[i]) {
+          card.removeClass('back');
+          card.addClass('hearts clickable ' + frontClasses[i]);
+          card.on('click', sendThisCard);
+        } else {
+          card.addClass('back');
+          card.removeClass('hearts clickable ' + frontClasses[i]);
+        }
+      }
+    }
+});
